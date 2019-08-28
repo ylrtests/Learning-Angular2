34e5c1758cac83230939e44a9798875b27a59860
Yojhan <yojhanr95@gmail.com>
2019-08-28 18:45:42 -0500

    Complemento ejemplo

--- app/component.app.ts
+++ app/component.app.ts
@@ -5,4 +5,32 @@ import { Component } from '@angular/core';
   templateUrl: './partials/app.html' 
 })
 
-export class AppComponent {}
+export class AppComponent {
+  //Estas declaraciones son opcionales y vienen de TypeScript
+  name: string; 
+  artists: string[];
+  students: any;
+  
+  constructor (){
+    this.name = "";
+    this.artists = [' Juanes', 'Fonseca', 'Shakira'];
+    this.students = [
+      {
+        name: "Carlos",
+        school: "Universidad Central"
+      },
+      {
+        name: "Jimenez",
+        school: "Universidad del Norte"
+      },
+      {
+        name: "Juan",
+        school: "Universidad del Sur"
+      },
+      {
+        name: "Paula",
+        school: "Universidad del Este"
+      }
+    ]
+  }
+}
