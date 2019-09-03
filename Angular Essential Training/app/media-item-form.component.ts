53ab2cd066b2813fead48dc8a690c7a9f0ede344
Yojhan <yojhanr95@gmail.com>
2019-09-03 09:33:51 -0500

    Add Template-driven form on Chapter 4

---
+++ Angular Essential Training/app/media-item-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mw-media-item-form',
+  templateUrl: 'app/media-item-form.component.html',
+  styleUrls: ['app/media-item-form.component.css']
+})
+export class MediaItemFormComponent {
+
+  onSubmit(mediaItem){
+    console.log(mediaItem)
+  }
+}
